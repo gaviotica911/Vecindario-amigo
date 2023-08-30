3fb9a21d5976324a0b09d2d094a3dcf2d59d64b3
Gabriela Garcia Suarez <g.garcias2@uniandes.edu.co>
2023-08-30 13:10:54 -0500

    repos vecino y publicacion

---
+++ src/main/java/co/edu/uniandes/dse/vecindarioamigo/repositories/PublicacionRepository.java
@@ -0,0 +1,19 @@
+package co.edu.uniandes.dse.vecindarioamigo.repositories;
+
+
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import co.edu.uniandes.dse.vecindarioamigo.entities.PublicacionEntity;
+
+
+@Repository
+public interface PublicacionRepository extends JpaRepository<PublicacionEntity, Long> {
+        List<PublicacionEntity> findByID(int ID);
+}
+
+
---
+++ src/main/java/co/edu/uniandes/dse/vecindarioamigo/repositories/VecinoRepository.java
@@ -0,0 +1,18 @@
+package co.edu.uniandes.dse.vecindarioamigo.repositories;
+
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import co.edu.uniandes.dse.vecindarioamigo.entities.VecinoEntity;
+
+
+@Repository
+public interface VecinoRepository extends JpaRepository<VecinoEntity, Long> {
+        List<VecinoEntity> findByID(int ID);
+}
+
+
