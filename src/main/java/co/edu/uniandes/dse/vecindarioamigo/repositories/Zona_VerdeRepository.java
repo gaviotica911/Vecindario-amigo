1482d0814be5e146b72e686f08d43b95bd0a1331
santinajar <s.najar@uniandes.edu.co>
2023-08-29 20:52:47 -0500

    Mejora clases negocio y zonas verdes

---
+++ src/main/java/co/edu/uniandes/dse/vecindarioamigo/repositories/Zona_VerdeRepository.java
@@ -0,0 +1,14 @@
+
+package co.edu.uniandes.dse.vecindarioamigo.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import co.edu.uniandes.dse.vecindarioamigo.entities.Zona_VerdeEntity;
+
+@Repository
+public interface  Zona_VerdeRepository extends JpaRepository<Zona_VerdeEntity, Long> {
+        
+}
