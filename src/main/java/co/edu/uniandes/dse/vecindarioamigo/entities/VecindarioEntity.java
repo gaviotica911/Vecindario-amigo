7a3fe6a562562b245d85f6ef74a090804dae6838
juanpablob11 <jp.baron@uniandes.edu.co>
2023-09-01 09:55:06 -0500

    Juan Pablo Changes

--- src/main/java/co/edu/uniandes/dse/vecindarioamigo/entities/VecindarioEntity.java
+++ src/main/java/co/edu/uniandes/dse/vecindarioamigo/entities/VecindarioEntity.java
@@ -3,7 +3,7 @@ package co.edu.uniandes.dse.vecindarioamigo.entities;
 import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import javax.persistence.CascadeType;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 
@@ -20,16 +20,21 @@ public class VecindarioEntity {
     @OneToMany(mappedBy = "habitantes", fetch = FetchType.LAZY)
     private List<VecinoEntity> habitantes = new ArrayList<>();
 
-    @OneToMany(mappedBy = "negocios", fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "negocios", fetch = FetchType.LAZY, orphanRemoval = true)
     private List<NegocioEntity> negocios = new ArrayList<>();
 
-    @OneToMany(mappedBy = "centrosComerciales", fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "centrosComerciales", fetch = FetchType.LAZY,
+               cascade = CascadeType.PERSIST, orphanRemoval = true)
     private List<CentroComercialEntity> centrosComerciales = new ArrayList<>();
 
-    @OneToMany(mappedBy = "vecindariosCercanos", fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "vecindariosCercanos", fetch = FetchType.LAZY,
+               cascade = CascadeType.PERSIST, orphanRemoval = true)
     private List<VecindarioEntity> vecindariosCercanos = new ArrayList<>();
 
-    @OneToMany(mappedBy = "zonasVerdes", fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "zonasVerdes", fetch = FetchType.LAZY,
+               cascade = CascadeType.PERSIST, orphanRemoval = true)
     private List<Zona_VerdeEntity> zonasVerdes = new ArrayList<>();
+
+
 
 }
