91900e3e2ede8938270fe9f4b699a46b3251749a
Gabriela Garcia Suarez <g.garcias2@uniandes.edu.co>
2023-09-01 10:44:00 -0500

    mark II

--- src/main/java/co/edu/uniandes/dse/vecindarioamigo/entities/VecinoEntity.java
+++ src/main/java/co/edu/uniandes/dse/vecindarioamigo/entities/VecinoEntity.java
@@ -20,7 +20,7 @@ public class VecinoEntity extends BaseEntity {
     private String porfile_pic;
     private String descripcion;
 
-    @ManyToMany(mappedBy = "vecino", fetch = FetchType.LAZY)
+    @ManyToMany(mappedBy = "miembros", fetch = FetchType.LAZY)
     private List<GruposDeInteresEntity> perteneceA = new ArrayList<>();
 
     @OneToMany(mappedBy = "vecino", fetch = FetchType.LAZY, cascade = CascadeType.PERSIST, orphanRemoval = true)
