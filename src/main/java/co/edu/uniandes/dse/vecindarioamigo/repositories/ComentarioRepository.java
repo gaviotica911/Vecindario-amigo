a44b13ab6793e0c5cbe40e48c95579f1e5d8f771
Juan-David-Obando-Novoa <111014101+Juan-David-Obando-Novoa@users.noreply.github.com>
2023-09-03 14:18:49 -0500

    RepoComentario

--- src/main/java/co/edu/uniandes/dse/vecindarioamigo/repositories/ComentarioRepository.java
+++ src/main/java/co/edu/uniandes/dse/vecindarioamigo/repositories/ComentarioRepository.java
@@ -1,13 +1,16 @@
 package co.edu.uniandes.dse.vecindarioamigo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import co.edu.uniandes.dse.vecindarioamigo.entities.CentroComercialEntity;
 import co.edu.uniandes.dse.vecindarioamigo.entities.ComentarioEntity;
 
 
 @Repository
 public interface ComentarioRepository extends JpaRepository<ComentarioEntity, Long> {
-    
+        List<ComentarioEntity> findByNombre(String nombre);
 
 }
