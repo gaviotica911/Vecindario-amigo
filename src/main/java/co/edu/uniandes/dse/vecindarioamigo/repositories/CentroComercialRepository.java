de9fa8a40d255d2271d2ac9acbae96bc4b879eda
Andres Serrano <a.serranoc@uniandes.edu.co>
2023-09-03 12:41:58 -0500

    CentroComercialService

--- src/main/java/co/edu/uniandes/dse/vecindarioamigo/repositories/CentroComercialRepository.java
+++ src/main/java/co/edu/uniandes/dse/vecindarioamigo/repositories/CentroComercialRepository.java
@@ -1,11 +1,13 @@
 package co.edu.uniandes.dse.vecindarioamigo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import co.edu.uniandes.dse.vecindarioamigo.entities.*;
 
-
 @Repository
 public interface CentroComercialRepository extends JpaRepository<CentroComercialEntity, Long> {
+    List<CentroComercialEntity> findByNombre(String nombre);
 }
--- src/main/java/co/edu/uniandes/dse/vecindarioamigo/services/CentroComercialService.java
+++ src/main/java/co/edu/uniandes/dse/vecindarioamigo/services/CentroComercialService.java
@@ -19,29 +19,29 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class CentroComercialService {
     @Autowired
-    CentroComercialRepository CentroComercialRepository;
+    CentroComercialRepository centroComercialRepository;
 
     @Transactional
-    public CentroComercialEntity createCentroComercial(CentroComercialEntity centroComercial)
+    public CentroComercialEntity createCentroComercial(CentroComercialEntity centroComercialEntity)
             throws EntityNotFoundException, IllegalOperationException {
 
-        if (!CentroComercialRepository.findById(centroComercial.getId()).isEmpty()) {
+        if (!centroComercialRepository.findByNombre(centroComercialEntity.getNombre()).isEmpty()) {
             throw new IllegalOperationException("Centro comercial name already exists");
         }
 
-        return CentroComercialRepository.save(centroComercial);
+        return centroComercialRepository.save(centroComercialEntity);
     }
 
     @Transactional
     public List<CentroComercialEntity> getCentrosComerciales() {
 
-        return CentroComercialRepository.findAll();
+        return centroComercialRepository.findAll();
     }
 
     @Transactional
     public CentroComercialEntity getCentroComercial(Long centroComercialID) throws EntityNotFoundException {
 
-        Optional<CentroComercialEntity> centroComercial = CentroComercialRepository.findById(centroComercialID);
+        Optional<CentroComercialEntity> centroComercial = centroComercialRepository.findById(centroComercialID);
         if (centroComercial.isEmpty())
             throw new EntityNotFoundException(ErrorMessage.SHOPPING_MALL_NOT_FOUND);
         return centroComercial.get();
@@ -50,20 +50,36 @@ public class CentroComercialService {
     @Transactional
     public CentroComercialEntity updateCentroComercial(Long centroComercialID, CentroComercialEntity centroComercial)
             throws EntityNotFoundException, IllegalOperationException {
-        Optional<CentroComercialEntity> centroComercial2 = CentroComercialRepository.findById(centroComercialID);
-        if (centroComercial2.isEmpty())
+        Optional<CentroComercialEntity> centroComercialEntity = centroComercialRepository.findById(centroComercialID);
+        if (centroComercialEntity.isEmpty())
             throw new EntityNotFoundException(ErrorMessage.SHOPPING_MALL_NOT_FOUND);
 
         centroComercial.setId(centroComercialID);
-        return CentroComercialRepository.save(centroComercial);
+        return centroComercialRepository.save(centroComercial);
     }
 
     @Transactional
     public void deleteCentroComercial(Long centroComercialID)
             throws EntityNotFoundException, IllegalOperationException {
-        Optional<CentroComercialEntity> centroComercial = CentroComercialRepository.findById(centroComercialID);
-        if (centroComercial.isEmpty())
+        log.info("Start process of deleting the shopping centre with id = {0}", centroComercialID);
+        Optional<CentroComercialEntity> centroComercialEntity = centroComercialRepository.findById(centroComercialID);
+        if (centroComercialEntity.isEmpty())
             throw new EntityNotFoundException(ErrorMessage.SHOPPING_MALL_NOT_FOUND);
-        CentroComercialRepository.deleteById(centroComercialID);
+
+        List<ComentarioEntity> comentarios = centroComercialEntity.get().getComentarios();
+        if (!comentarios.isEmpty()) {
+            throw new IllegalOperationException(
+                    "Unable to delete 'centro comercial' because it has associated comments");
+        }
+
+        List<NegocioEntity> negocios = centroComercialEntity.get().getLista_negocios();
+        if (!negocios.isEmpty()) {
+            throw new IllegalOperationException(
+                    "Unable to delete 'centro comercial' because it has associated businesses");
+        }
+        log.info("End process of checking the neighborhood constraints with id = {0}", centroComercialID);
+
+        centroComercialRepository.deleteById(centroComercialID);
+        log.info("End process of deleting the neighborhood with id = {0}", centroComercialID);
     }
 }
