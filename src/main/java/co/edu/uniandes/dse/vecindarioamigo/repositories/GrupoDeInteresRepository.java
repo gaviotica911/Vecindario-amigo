f99d0ac9d8a59f48eae2629ee6819ebddfbdf769
Juan-David-Obando-Novoa <111014101+Juan-David-Obando-Novoa@users.noreply.github.com>
2023-09-03 14:14:59 -0500

    .

--- src/main/java/co/edu/uniandes/dse/vecindarioamigo/repositories/GrupoDeInteresRepository.java
+++ src/main/java/co/edu/uniandes/dse/vecindarioamigo/repositories/GrupoDeInteresRepository.java
@@ -1,13 +1,16 @@
 package co.edu.uniandes.dse.vecindarioamigo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import co.edu.uniandes.dse.vecindarioamigo.entities.GruposDeInteresEntity;
+import co.edu.uniandes.dse.vecindarioamigo.entities.NegocioEntity;
 
 
 @Repository
 public interface GrupoDeInteresRepository extends JpaRepository<GruposDeInteresEntity, Long> {
-   
+     List<GruposDeInteresEntity> findByNombre(String nombre);  
 
 }
