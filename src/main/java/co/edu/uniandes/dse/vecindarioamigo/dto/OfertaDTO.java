d882e0d81262f5625f91ccfb128029054145ccd5
juanpablob11 <jp.baron@uniandes.edu.co>
2023-09-30 08:24:19 -0500

    mark dto  without detail

--- src/main/java/co/edu/uniandes/dse/vecindarioamigo/dto/OfertaDTO.java
+++ src/main/java/co/edu/uniandes/dse/vecindarioamigo/dto/OfertaDTO.java
@@ -1,5 +1,13 @@
 package co.edu.uniandes.dse.vecindarioamigo.dto;
 
-public class OfertaDTO {
+import co.edu.uniandes.dse.vecindarioamigo.entities.NegocioEntity;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+public class OfertaDTO {
+    private Long id;
+    private String descripcion;
+    private NegocioEntity negocio;
 }
--- src/main/java/co/edu/uniandes/dse/vecindarioamigo/dto/VecindarioDTO.java
+++ src/main/java/co/edu/uniandes/dse/vecindarioamigo/dto/VecindarioDTO.java
@@ -6,6 +6,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class VecindarioDTO {
-
     private Long id;
+    private String nombre;
+    private String ciudad;
+    private String localidad;
 }
