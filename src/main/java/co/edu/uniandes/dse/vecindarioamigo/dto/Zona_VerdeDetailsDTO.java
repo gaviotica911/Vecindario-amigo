2b327dd4552058da2a0cd50fc886bf1bd01489dc
Gabriela Garcia <g.garcias2@uniandes.edu.co>
2023-11-14 09:00:17 -0500

    correccion zona verde

--- src/main/java/co/edu/uniandes/dse/vecindarioamigo/dto/Zona_VerdeDetailsDTO.java
+++ src/main/java/co/edu/uniandes/dse/vecindarioamigo/dto/Zona_VerdeDetailsDTO.java
@@ -3,10 +3,13 @@ package co.edu.uniandes.dse.vecindarioamigo.dto;
 import java.util.ArrayList;
 import java.util.List;
 
-import lombok.Data;
 
-@Data
-public class Zona_VerdeDetailsDTO {
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Zona_VerdeDetailsDTO extends Zona_VerdeDTO{
 
     private List<ComentarioDTO> comentarios = new ArrayList<>();
 
